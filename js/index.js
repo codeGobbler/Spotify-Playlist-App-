62c28deebcec9f424f49d46e9975f90e4f546cc6
Alton Michaux <michauxboyz@yahoo.com>
2021-02-03 01:55:24 +0000

    setup eventlisteners on all clickable buttons to ensure they work

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
-const { default: SpotifyWebApi } = require('spotify-web-api-js');
-const spotify = require('spotify-web-api-js');
-const s = new SpotifyWebApi();
+// const { default: SpotifyWebApi } = require('spotify-web-api-js');
+// const spotify = require('spotify-web-api-js');
+// const s = new SpotifyWebApi();
+
+//menu button setup
+const menuBtn = document.getElementById('btn')
+  .addEventListener('click', (e) => {
+    console.log('menu clicked');
+})
+
+//login button setup
+const loginBtn = document.getElementById('spotify-btn')
+  .addEventListener('click', (e) => {
+    console.log('login clicked');
+})
+
+//track-select button setup
+const currentTrack = document.getElementById('current')
+  .addEventListener('click', () => {
+    console.log('current track clicked');
+})
+
+const prevTrack = document.getElementById('prev')
+  .addEventListener('click', () => {
+    console.log('previous track clicked');
+})
+
+const nextTrack = document.getElementById('next')
+  .addEventListener('click', () => {
+    console.log('next track clicked');
+})
+//playlist-art button setup
