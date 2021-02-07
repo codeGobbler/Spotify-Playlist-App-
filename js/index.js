44f387b81e38d6c5778c2f9fa9895b2e0c3f8b6b
Alton Michaux <alton.michaux@yahoo.com>
2021-02-07 11:37:09 -0500

    started writing some html to display on page

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@
 //---------API Controller------------//
 //-----------------------------------//
 const apiController = (function () {
-  const clientId ="";
-  const clientSecret ="";
-  const userId ="";
+  const clientId = "";
+  const clientSecret = "";
+  const userId = "";
 
   const getToken = async () => {
     const result = await fetch("https://accounts.spotify.com/api/token", {
@@ -18,7 +18,7 @@ const apiController = (function () {
     // console.log(result);
 
     const data = await result.json();
-    // console.log(data);
+    console.log(data);
     return data.access_token;
   };
 
@@ -30,10 +30,9 @@ const apiController = (function () {
       {
         method: "GET",
         headers: {
-          Accept: "appliction/json",
+          Accept: "application/json",
           "Content-Type": "application/json",
-          Authorization:
-            "Bearer BQCqoqdyTJXPS53Ty7yvNiiJhIuepJd1kEXeA7hfnEunK8cu6EZ4cCQ3qelMWuGrF9pU2QI_Zuiny7PRjccjqrgkcVW39A_5TIF2HRMTE95Opk0Y7ovL940wp8ZMrJpsOKFzO3DYbiOlhd9XNEafog_GaSvYJf5T",
+          Authorization: `Bearer ${token}`,
         },
       }
     );
@@ -55,6 +54,7 @@ const apiController = (function () {
 
 const uiController = (function () {
   const domElements = {
+    songDetail: "#song-descripiton",
     currentSong: "#current",
     previousSong: "#prev",
     nextSong: "#next",
@@ -62,11 +62,13 @@ const uiController = (function () {
     nowPlaying: "#now-playing",
     playlistContents: "#metadata-1",
     otherPlaylists: "#metadata-2",
+    genreSelect: "#genre-select"
   };
 
   return {
     inputField() {
       return {
+        songDetail: document.querySelector(domElements.songDetail),
         currentSong: document.querySelector(domElements.currentSong),
         previousSong: document.querySelector(domElements.previousSong),
         nextSong: document.querySelector(domElements.nextSong),
@@ -74,7 +76,35 @@ const uiController = (function () {
         nowPlaying: document.querySelector(domElements.nowPlaying),
         playlistSongs: document.querySelector(domElements.playlistContents),
         playlistLibrary: document.querySelector(domElements.otherPlaylists),
+        genreSelect: document.querySelector(domElements.genreSelect)
       };
     },
-  };
+
+  genreMenu (value, text) {
+    const newHTML = `<option value=${value}>${text}</option>`
+    document.querySelector(domElements.genreSelect).insertAdjacentHTML('beforeend', html)
+  },
+
+  createTrackDetail (img, title, artist) {
+
+    const songDiv = document.querySelector(domElements.songDetail);
+    // any time user clicks a new song, we need to clear out the song detail div
+    songDiv.innerHTML = '';
+
+    const html = 
+    `
+    <div class="track-description">
+        <img src="${img}" alt="">        
+    </div>
+    <div class="track-description">
+        <label for="Genre" class="track-description">${title}:</label>
+    </div>
+    <div class="track-description">
+        <label for="artist" class="track-description">By ${artist}:</label>
+    </div> 
+    `;
+
+    songDiv.insertAdjacentHTML('beforeend', html)
+}
+  }
 })();
